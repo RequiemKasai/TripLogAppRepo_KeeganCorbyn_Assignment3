e06bfefbeca7c58c843160a7367735d7f8d585e0
Corbyn <144940013+Blueracey@users.noreply.github.com>
2024-11-13 23:21:41 -0700

    Added input validation of the trip model as well as set certain columns to requires/nullable

--- Models/Trip.cs
+++ Models/Trip.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TripsLogApp.Models
 {
@@ -7,14 +8,26 @@ namespace TripsLogApp.Models
     {
         [Key]
         public int TripId { get; set; }
+        [Required]
         public string Destination { get; set; }
+        [Required]
         public DateTime StartDate { get; set; }
+        [Required]
         public DateTime EndDate { get; set; }
+        [AllowNull]
+
         public string Accommodation { get; set; }
+        [AllowNull]
+        [RegularExpression(@"^(?([0-9]{3}))?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",ErrorMessage ="Requires valid phone number")]
         public string AccommodationPhone { get; set; }
+        [AllowNull]
+        [EmailAddress(ErrorMessage ="requires valid email address")]
         public string AccommodationEmail { get; set; }
+        [AllowNull]
         public string ThingToDo1 { get; set; }
+        [AllowNull]
         public string ThingToDo2 { get; set; }
+        [AllowNull]
         public string ThingToDo3 { get; set; }
     }
 }
