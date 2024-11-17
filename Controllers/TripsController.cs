23132b650b8c705c05131d5937947c447d0019ec
Corbyn <144940013+Blueracey@users.noreply.github.com>
2024-11-17 00:54:14 -0700

    Create now works, mostly untested though

--- Controllers/TripsController.cs
+++ Controllers/TripsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -54,25 +55,27 @@ namespace TripLogApp_KeeganCorbyn_Assignment3.Controllers
         // POST: Trips/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-      //  [ValidateAntiForgeryToken]
-        //public async Task<IActionResult> AddPage1([Bind("TripId,Destination,StartDate,EndDate,Accommodation,AccommodationPhone,AccommodationEmail,ThingToDo1,ThingToDo2,ThingToDo3")] Trip trip)
-       // {
-       //     if (ModelState.IsValid)
-      //      {
-                // _context.Add(trip);
-                // await _context.SaveChangesAsync();
+        [HttpPost]
+
+        public async Task<IActionResult> Page3([Bind("TripId,Destination,StartDate,EndDate,Accommodation,AccommodationPhone,AccommodationEmail,ThingToDo1,ThingToDo2,ThingToDo3")] Trip trip)
+        {
+
+            Console.WriteLine("test");
+            _context.Add(trip);
+             await _context.SaveChangesAsync();
+             TempData["AddedTrip"] = "Trip to" +TempData["Destination"] ;
                 
-        //        return RedirectToAction(nameof(Page2));
-       //     }
-       //     return View(trip);
-     //   }
+                return RedirectToAction(nameof(Index));
+            
+            
+            
+        }
 
         public IActionResult FormPage1(string Destination,string? Accommodation, DateOnly StartDate, DateOnly EndDate)
         {
             if (ModelState.IsValid)
             {
-                
+                Debug.Write("works?");
                 TempData["Destination"] = Destination;
                 TempData["Accommodation"] = Accommodation;
                 TempData["StartDate"] = StartDate.ToString();
@@ -114,30 +117,71 @@ namespace TripLogApp_KeeganCorbyn_Assignment3.Controllers
             }
         }
 
-        public IActionResult Page3() { 
+        public IActionResult Page3() {
+
+
+            string? StartDate = TempData["StartDate"] as string;
+            string? EndDate = TempData["EndDate"] as string;
+
+            try
+            {
+                ViewBag.Destination = TempData["Destination"];
+            }
+            catch
+            {
+                ViewBag.Destination = null;
+            }
+
+            try
+            {
+                ViewBag.StartDate = DateOnly.Parse(StartDate);
+            }
+            catch
+            {
+                ViewBag.StartDate = null;  
+            }
+
+
+            try
+            {
+                ViewBag.EndDate = DateOnly.Parse(EndDate);
+            }
+            catch 
+            { 
+                ViewBag.EndDate = null; 
+            }
+            try
+            {
+                ViewBag.Accommodation = TempData["Accommodation"];
+            }
+            catch
+            { 
+                ViewBag.Accommodation = null;
+            }
+            try
+            {
+                ViewBag.AccommodationPhone = TempData["AccommodationPhone"];
+            }
+            catch
+            {
+                ViewBag.AccommodationPhone = null;
+            }
+
+            try
+            {
+                ViewBag.AccommodationEmail = TempData["AccommodationEmail"];
+            }
+            catch
+            {
+                ViewBag.AccommodationEmail = null;
+            }
+            
+            
+
             return View();
         }
 
-        public IActionResult FormPage3(string? ThingToDo1, string? ThingToDo2, string? ThingToDo3)
-        {
-
-            if (ModelState.IsValid)
-            {
-                
-                TempData["ThingToDo1"] = ThingToDo1;
-                TempData["ThingToDo2"] = ThingToDo2;
-                TempData["ThingToDo3"] = ThingToDo3;
-
-
-                TempData["AddedTrip"] = TempData["Destination"]; ;
-
-                return RedirectToAction(nameof(Index));
-            }
-            else
-            {
-                return View();
-            }
-        }
+        
 
        
 
@@ -157,73 +201,12 @@ namespace TripLogApp_KeeganCorbyn_Assignment3.Controllers
             return View(trip);
         }
 
-        // POST: Trips/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("TripId,Destination,StartDate,EndDate,Accommodation,AccommodationPhone,AccommodationEmail,ThingToDo1,ThingToDo2,ThingToDo3")] Trip trip)
-        {
-            if (id != trip.TripId)
-            {
-                return NotFound();
-            }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(trip);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TripExists(trip.TripId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(trip);
-        }
+       
 
-        // GET: Trips/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+       
 
-            var trip = await _context.Trips
-                .FirstOrDefaultAsync(m => m.TripId == id);
-            if (trip == null)
-            {
-                return NotFound();
-            }
 
-            return View(trip);
-        }
-
-        // POST: Trips/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var trip = await _context.Trips.FindAsync(id);
-            if (trip != null)
-            {
-                _context.Trips.Remove(trip);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
 
         private bool TripExists(int id)
         {
